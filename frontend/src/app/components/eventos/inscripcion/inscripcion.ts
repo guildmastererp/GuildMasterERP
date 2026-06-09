5ed6b755ded4b360187d034a380184d6cf717498
MichelleGP <guildmastererp@gmailcom>
2026-06-09 18:09:48 +0000

    Correción vista de inscripción de eventos

--- frontend/src/app/components/eventos/inscripcion/inscripcion.ts
+++ frontend/src/app/components/eventos/inscripcion/inscripcion.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-inscripcion-eventos',
@@ -18,36 +19,56 @@ export class Inscripcion implements OnInit {
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
-    this.cargarMisPersonajes();
-    this.cargarEventosProximos();
+    this.cargarDatosIniciales(); 
   }
 
   private getHeaders() {
     return new HttpHeaders({ 'Authorization': `Bearer ${localStorage.getItem('token')}`, 'Accept': 'application/json' });
   }
 
-  cargarMisPersonajes() {
-    this.http.get<any[]>('http://192.168.1.130:8000/api/mis-personajes', { headers: this.getHeaders() })
-      .subscribe({
-        next: (data) => {
-          this.misPersonajes = data;
-          this.cdr.detectChanges();
-        },
-        error: () => this.cdr.detectChanges()
-      });
+  cargarDatosIniciales() {
+    const peticionPersonajes = this.http.get<any[]>('http://192.168.1.130:8000/api/mis-personajes', { headers: this.getHeaders() });
+    const peticionEventos = this.http.get<any[]>('http://192.168.1.130:8000/api/eventos/proximos', { headers: this.getHeaders() });
+
+    forkJoin({
+      personajes: peticionPersonajes,
+      eventos: peticionEventos
+    }).subscribe({
+      next: (res) => {
+        this.misPersonajes = res.personajes;
+        
+        // Formateamos los eventos antes de dárselos al HTML
+        this.eventosProximos = res.eventos.map((e: any) => {
+          // 1. Arreglamos la fecha de Laravel (espacio por T) para que Angular no falle
+          if (e.fecha_evento) {
+            e.fecha_evento_limpia = e.fecha_evento.replace(' ', 'T');
+          }
+          // 2. Pre-calculamos la inscripción para no saturar el HTML
+          e.miInscripcion = this.obtenerInscripcionLocal(e, this.misPersonajes);
+          return e;
+        });
+        
+        this.eventosProximos.forEach(e => {
+          if (this.misPersonajes.length > 0) {
+            const main = this.misPersonajes.find(p => p.es_main == 1 || p.es_main === true);
+            this.personajeSeleccionado[e.codigo] = main ? main.codigo : this.misPersonajes[0].codigo;
+          }
+        });
+        
+        this.cdr.detectChanges();
+      },
+      error: () => this.cdr.detectChanges()
+    });
   }
 
   cargarEventosProximos() {
     this.http.get<any[]>('http://192.168.1.130:8000/api/eventos/proximos', { headers: this.getHeaders() })
       .subscribe({
         next: (data) => {
-          this.eventosProximos = data;
-          
-          this.eventosProximos.forEach(e => {
-            if (this.misPersonajes.length > 0) {
-              const main = this.misPersonajes.find(p => p.es_main);
-              this.personajeSeleccionado[e.codigo] = main ? main.codigo : this.misPersonajes[0].codigo;
-            }
+          this.eventosProximos = data.map((e: any) => {
+            if (e.fecha_evento) e.fecha_evento_limpia = e.fecha_evento.replace(' ', 'T');
+            e.miInscripcion = this.obtenerInscripcionLocal(e, this.misPersonajes);
+            return e;
           });
           this.cdr.detectChanges();
         },
@@ -55,10 +76,10 @@ export class Inscripcion implements OnInit {
       });
   }
 
-  obtenerMiInscripcion(evento: any): any {
-    if (!evento.inscritos || this.misPersonajes.length === 0) return null;
-    
-    for (let miPj of this.misPersonajes) {
+  // Método auxiliar interno que no llamamos desde el HTML
+  private obtenerInscripcionLocal(evento: any, misPjs: any[]): any {
+    if (!evento.inscritos || misPjs.length === 0) return null;
+    for (let miPj of misPjs) {
       const inscrito = evento.inscritos.find((i: any) => i.codigo === miPj.codigo);
       if (inscrito) return inscrito; 
     }
@@ -80,7 +101,6 @@ export class Inscripcion implements OnInit {
         next: () => {
           this.cargarEventosProximos(); 
           this.procesando = false; 
-          this.cdr.detectChanges();
         },
         error: () => {
           this.procesando = false; 
@@ -97,7 +117,6 @@ export class Inscripcion implements OnInit {
         next: () => {
           this.cargarEventosProximos();
           this.procesando = false; 
-          this.cdr.detectChanges();
         },
         error: () => {
           this.procesando = false; 
